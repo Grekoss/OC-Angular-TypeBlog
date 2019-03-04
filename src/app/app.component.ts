6206bc092634a474c82952519b830db43c592393
Cédric Pourrias <pourriascedric@gmail.com>
2019-03-04 21:46:55 +0100

    Create Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-blog';
+
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: 'Lorem ipsum dolor sit amet. Accusamus adipisci aliquam asperiores cum error fugiat necessitatibus nihil quam.',
+      loveIts: 0,
+      created_at: new Date(),
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Error esse fugiat iste rem soluta tenetur veniam voluptatem! Eum, maxime, rem. Architecto excepturi iusto nemo quos.',
+      loveIts: 0,
+      created_at: new Date(),
+    },
+    {
+      title: 'Mon troisième post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum ea earum mollitia repellendus.',
+      loveIts: 0,
+      created_at: new Date(),
+    },
+    {
+      title: 'Mon quatrième post',
+      content: 'Lorem vide ;)',
+      loveIts: 0,
+      created_at: new Date(),
+    }
+  ];
 }
